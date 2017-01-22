9fbe426ebd561f1a2536f67c0a16118dede1b9ba
w-rudolph <172247097@qq.com>
2017-01-22 14:55:11 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var BUI;
+(function (BUI) {
+    var ModalSize;
+    (function (ModalSize) {
+        ModalSize[ModalSize["SM"] = 0] = "SM";
+        ModalSize[ModalSize["MD"] = 1] = "MD";
+        ModalSize[ModalSize["LG"] = 2] = "LG";
+    })(ModalSize || (ModalSize = {}));
+    var dialogBox = (function () {
+        function dialogBox(options) {
+            this.options = {
+                title: 'title',
+                content: 'content',
+                backdrop: true,
+                vCenter: false,
+                container: 'body',
+                size: ModalSize.MD,
+                selector: '#dialogBox'
+            };
+            this.options = $.extend(this.options, options);
+            this.selector = this.options.selector;
+            this.contents = [];
+            this.render();
+        }
+        dialogBox.prototype.render = function () {
+            var _this = this;
+            if ($(this.selector).length)
+                return;
+            $(this.options.container).append(this.renderTpl());
+            var $dialogBox = $(this.selector);
+            $dialogBox.find('.modal-dialog').css('opacity', 0);
+            this.updatePosition();
+            $dialogBox.modal(this.options).on('shown.bs.modal', function () {
+                $dialogBox.find('.modal-dialog').css('opacity', 1);
+                _this.updatePosition();
+            });
+        };
+        dialogBox.prototype.show = function () {
+            var $dialogBox = $(this.selector);
+            $dialogBox.modal('show');
+        };
+        dialogBox.prototype.hide = function () {
+            var $dialogBox = $(this.selector);
+            $dialogBox.modal('hide');
+        };
+        dialogBox.prototype.push = function (content) {
+            var $dialogBox = $(this.selector);
+            var $body = $dialogBox.find('.modal-body');
+            this.contents.push($body.html());
+            this.update(content);
+            return this;
+        };
+        dialogBox.prototype.pop = function () {
+            if (this.contents.length)
+                this.update(this.contents.pop());
+            else
+                this.hide();
+            return this;
+        };
+        dialogBox.prototype.clear = function () {
+            this.contents = [];
+        };
+        dialogBox.prototype.destory = function () {
+            this.hide();
+            $(this.selector).remove();
+        };
+        dialogBox.prototype.update = function (content) {
+            var $dialogBox = $(this.selector);
+            var $body = $dialogBox.find('.modal-body');
+            $body.html(content);
+            this.updatePosition();
+        };
+        dialogBox.prototype.renderTpl = function () {
+            return "<div class=\"modal fade\" data-show=\"false\" id=\"" + this.selector.substring(1) + "\">\n                        <div class=\"modal-dialog  modal-" + ModalSize[this.options.size].toLowerCase() + "\">\n                            <div class=\"modal-content\">\n                                <div class=\"modal-header\">\n                                  <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>\n                                  <h4 class=\"modal-title\">" + this.options.title + "</h4>\n                                </div>\n                                <div class=\"modal-body\">" + this.options.content + "</div>\n                            </div>\n                        </div>\n                     </div>";
+        };
+        dialogBox.prototype.updatePosition = function () {
+            var $dialogBox = $(this.selector);
+            var wH = $(window).height();
+            var H = $dialogBox.find('.modal-dialog').outerHeight(true);
+            $dialogBox.find('.modal-dialog').css({ 'transform': "translate(0, " + (wH - H > 0 ? wH - H : 0) / 2 + "px)" });
+        };
+        return dialogBox;
+    }());
+    BUI.dialogBox = dialogBox;
+    var alertBox = (function (_super) {
+        __extends(alertBox, _super);
+        function alertBox(options) {
+            var _this = this;
+            options.selector = options.selector || '#alertBox';
+            options.submitTxt = options.submitTxt || 'submit';
+            options.submitTxtLoadingTxt = options.submitTxtLoadingTxt || 'loading...';
+            _super.call(this, options);
+            var $alertBox = $(this.selector);
+            $alertBox.on('click', '.submit', function () {
+                if (typeof _this.options.callback === 'function') {
+                    _this.options.callback.call(null, _this, $alertBox.find('.submit'));
+                }
+            });
+        }
+        alertBox.prototype.renderTpl = function () {
+            return "<div class=\"modal fade\" data-show=\"false\" id=\"" + this.selector.substring(1) + "\">\n                        <div class=\"modal-dialog  modal-" + ModalSize[this.options.size].toLowerCase() + "\">\n                            <div class=\"modal-content\">\n                                <div class=\"modal-header\">\n                                  <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>\n                                  <h4 class=\"modal-title\">" + this.options.title + "</h4>\n                                </div>\n                                <div class=\"modal-body\">" + this.options.content + "</div>\n                                <div class=\"modal-footer\">\n                                   <button type=\"button\" class=\"btn btn-primary submit\" data-loading-text=\"" + this.options.submitTxtLoadingTxt + "\">" + this.options.submitTxt + "</button>      \n                                </div>\n                            </div>\n                        </div>\n                     </div>";
+        };
+        return alertBox;
+    }(dialogBox));
+    BUI.alertBox = alertBox;
+    var confirmBox = (function (_super) {
+        __extends(confirmBox, _super);
+        function confirmBox(options) {
+            var _this = this;
+            options.selector = options.selector || '#confirmBox';
+            options.cancelTxt = options.cancelTxt || 'cancel';
+            _super.call(this, options);
+            var $confirmBox = $(this.selector);
+            $confirmBox.off('click').on('click', '.submit, .cancel', function (e) {
+                if (typeof _this.options.callback === 'function') {
+                    _this.options.callback.call(null, _this, $(e.currentTarget));
+                }
+            });
+        }
+        confirmBox.prototype.renderTpl = function () {
+            return "<div class=\"modal fade\" data-show=\"false\" id=\"" + this.selector.substring(1) + "\">\n                        <div class=\"modal-dialog  modal-" + ModalSize[this.options.size].toLowerCase() + "\">\n                            <div class=\"modal-content\">\n                                <div class=\"modal-header\">\n                                  <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>\n                                  <h4 class=\"modal-title\">" + this.options.title + "</h4>\n                                </div>\n                                <div class=\"modal-body\">" + this.options.content + "</div>\n                                <div class=\"modal-footer\">\n                                   <button type=\"button\" class=\"btn btn-default cancel\" data-op=\"cancel\">" + this.options.cancelTxt + "</button>   \n                                   <button type=\"button\" class=\"btn btn-primary submit\" data-op=\"submit\"  data-loading-text=\"" + this.options.submitTxtLoadingTxt + "\">" + this.options.submitTxt + "</button> \n                                </div>\n                            </div>\n                        </div>\n                     </div>";
+        };
+        return confirmBox;
+    }(alertBox));
+    BUI.confirmBox = confirmBox;
+})(BUI || (BUI = {}));
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,169 @@
+﻿declare var $;
+namespace BUI {
+    enum ModalSize {
+       SM, MD, LG
+    }
+    interface Option {
+        title: string;
+        content: string;
+        backdrop: boolean;
+        vCenter: boolean;
+        container: string;
+        size: ModalSize;
+        selector: string;
+    }
+    export class dialogBox {
+        options: Option;
+        protected selector: string;
+        protected contents: Array<any>;
+        constructor(options: Option) {
+            this.options = <Option>{
+                title: 'title',
+                content: 'content',
+                backdrop: true,
+                vCenter: false,
+                container: 'body',
+                size: ModalSize.MD,
+                selector: '#dialogBox'
+            };
+            this.options = $.extend(this.options, options);
+            this.selector = this.options.selector;
+            this.contents = [];
+            this.render();
+        }
+        render() {
+            if ($(this.selector).length) return;
+            $(this.options.container).append(this.renderTpl());
+            const $dialogBox = $(this.selector);
+            $dialogBox.find('.modal-dialog').css('opacity', 0);
+            this.updatePosition(); 
+            $dialogBox.modal(this.options).on('shown.bs.modal',() => {
+                  $dialogBox.find('.modal-dialog').css('opacity', 1);
+                  this.updatePosition();
+            });
+        }
+        show(){
+           const $dialogBox = $(this.selector);
+           $dialogBox.modal('show');
+        }
+        hide(){
+           const $dialogBox = $(this.selector);
+           $dialogBox.modal('hide');
+        }
+        push(content: any){
+           const $dialogBox = $(this.selector);
+           const $body = $dialogBox.find('.modal-body');
+           this.contents.push($body.html());
+           this.update(content);
+           return this;
+        }
+        pop(){
+           if(this.contents.length)this.update(this.contents.pop());
+           else this.hide();
+           return this;
+        }
+        clear(){
+          this.contents = [];
+        }
+        destory(){
+          this.hide();
+          $(this.selector).remove();
+        }
+        update(content: any){
+           const $dialogBox = $(this.selector);
+           const $body = $dialogBox.find('.modal-body');
+           $body.html(content);
+           this.updatePosition();
+        }
+        renderTpl(): string {
+            return `<div class="modal fade" data-show="false" id="${this.selector.substring(1)}">
+                        <div class="modal-dialog  modal-${ModalSize[this.options.size].toLowerCase()}">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                  <h4 class="modal-title">${this.options.title}</h4>
+                                </div>
+                                <div class="modal-body">${this.options.content}</div>
+                            </div>
+                        </div>
+                     </div>`;
+        }
+        updatePosition(){
+             const $dialogBox = $(this.selector);
+             var wH = $(window).height();
+             var H = $dialogBox.find('.modal-dialog').outerHeight(true);
+             $dialogBox.find('.modal-dialog').css({'transform': `translate(0, ${(wH - H > 0 ? wH - H : 0)/2}px)`});
+        }
+    }
+    interface AlertOption extends Option{
+        submitTxt: string;
+        submitTxtLoadingTxt: string;
+        callback: any;
+    }
+    export class alertBox extends dialogBox{
+        options: AlertOption;
+        constructor(options: AlertOption){
+            options.selector =  options.selector || '#alertBox';
+            options.submitTxt = options.submitTxt || 'submit';
+            options.submitTxtLoadingTxt = options.submitTxtLoadingTxt || 'loading...';
+            super(options);
+            var $alertBox = $(this.selector);
+            $alertBox.on('click', '.submit', () => {
+               if(typeof this.options.callback === 'function'){
+                  this.options.callback.call(null, this, $alertBox.find('.submit'));
+               }
+            })
+        }
+        renderTpl(){
+            return `<div class="modal fade" data-show="false" id="${this.selector.substring(1)}">
+                        <div class="modal-dialog  modal-${ModalSize[this.options.size].toLowerCase()}">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                  <h4 class="modal-title">${this.options.title}</h4>
+                                </div>
+                                <div class="modal-body">${this.options.content}</div>
+                                <div class="modal-footer">
+                                   <button type="button" class="btn btn-primary submit" data-loading-text="${this.options.submitTxtLoadingTxt}">${this.options.submitTxt}</button>      
+                                </div>
+                            </div>
+                        </div>
+                     </div>`;
+        }
+    }
+    interface ConfirmOption extends AlertOption{
+        cancelTxt: string;
+    }
+    export class confirmBox extends alertBox{
+       options: ConfirmOption;
+       constructor(options: ConfirmOption){
+         options.selector =  options.selector || '#confirmBox';
+         options.cancelTxt =  options.cancelTxt || 'cancel';
+         super(options);
+         var $confirmBox = $(this.selector);
+         $confirmBox.off('click').on('click', '.submit, .cancel', (e) => {
+            if(typeof this.options.callback === 'function'){
+                  this.options.callback.call(null, this, $(e.currentTarget));
+            }
+         })
+       }
+       renderTpl(){
+            return `<div class="modal fade" data-show="false" id="${this.selector.substring(1)}">
+                        <div class="modal-dialog  modal-${ModalSize[this.options.size].toLowerCase()}">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                  <h4 class="modal-title">${this.options.title}</h4>
+                                </div>
+                                <div class="modal-body">${this.options.content}</div>
+                                <div class="modal-footer">
+                                   <button type="button" class="btn btn-default cancel" data-op="cancel">${this.options.cancelTxt}</button>   
+                                   <button type="button" class="btn btn-primary submit" data-op="submit"  data-loading-text="${this.options.submitTxtLoadingTxt}">${this.options.submitTxt}</button> 
+                                </div>
+                            </div>
+                        </div>
+                     </div>`;
+        }
+    }
+
+}
